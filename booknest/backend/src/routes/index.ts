98829da79af54791f46f8b68ed12761037b8190e
zyf <2821432166@qq.com>
2026-04-30 14:32:07 +0800

    feat: Category CRUD

--- booknest/backend/src/routes/index.ts
+++ booknest/backend/src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import authRoutes from './auth.routes'
 import bookRoutes from './book.routes'
+import categoryRoutes from './category.routes'
 
 const router = Router()
 router.use('/auth', authRoutes)
 router.use('/books', bookRoutes)
+router.use('/categories', categoryRoutes)
 
 export default router
