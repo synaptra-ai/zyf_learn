7990497d3929062aef94718f7f3c1d90cd977bfa
zyf <2821432166@qq.com>
2026-04-30 15:26:34 +0800

    feat: 添加 React Query 配置和缓存键工厂

--- booknest/frontend/src/App.tsx
+++ booknest/frontend/src/App.tsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from '@/lib/query-client'
 import Layout from '@/components/Layout'
 import ProtectedRoute from '@/components/ProtectedRoute'
 import BookList from '@/pages/BookList'
@@ -12,20 +14,22 @@ import Register from '@/pages/Register'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/" element={<ProtectedRoute><Layout /></ProtectedRoute>}>
-          <Route index element={<BookList />} />
-          <Route path="books/new" element={<BookCreate />} />
-          <Route path="books/:id" element={<BookDetail />} />
-          <Route path="books/:id/edit" element={<BookEdit />} />
-          <Route path="categories" element={<CategoryManager />} />
-          <Route path="stats" element={<Stats />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<ProtectedRoute><Layout /></ProtectedRoute>}>
+            <Route index element={<BookList />} />
+            <Route path="books/new" element={<BookCreate />} />
+            <Route path="books/:id" element={<BookDetail />} />
+            <Route path="books/:id/edit" element={<BookEdit />} />
+            <Route path="categories" element={<CategoryManager />} />
+            <Route path="stats" element={<Stats />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
 
