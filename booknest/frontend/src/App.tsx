3a24be2e5dc591a28e6a1de7d7598a464dc87324
zyf <2821432166@qq.com>
2026-04-30 15:23:42 +0800

    feat: 实现登录注册页面和路由守卫

--- booknest/frontend/src/App.tsx
+++ booknest/frontend/src/App.tsx
@@ -1,17 +1,22 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from '@/components/Layout'
+import ProtectedRoute from '@/components/ProtectedRoute'
 import BookList from '@/pages/BookList'
 import BookCreate from '@/pages/BookCreate'
 import BookDetail from '@/pages/BookDetail'
 import BookEdit from '@/pages/BookEdit'
 import CategoryManager from '@/pages/CategoryManager'
 import Stats from '@/pages/Stats'
+import Login from '@/pages/Login'
+import Register from '@/pages/Register'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<ProtectedRoute><Layout /></ProtectedRoute>}>
           <Route index element={<BookList />} />
           <Route path="books/new" element={<BookCreate />} />
           <Route path="books/:id" element={<BookDetail />} />
